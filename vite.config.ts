29dfe3107a615223e388a0cd951de47989e8992a
Matvey <morozovmatvej75@gmail.com>
2023-02-26 13:25:11 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./secrets.local"
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {}
+  }
 })
